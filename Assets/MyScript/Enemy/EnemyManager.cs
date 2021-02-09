287143f11f337030c296da4a278e6ae499b88bd9
as <>
2021-02-09 09:31:29 +0900

    攻撃を受けると移動出来てしまうバグの修正

--- Assets/MyScript/Enemy/EnemyManager.cs
+++ Assets/MyScript/Enemy/EnemyManager.cs
@@ -8,18 +8,40 @@ public class EnemyManager : MonoBehaviour
     public Transform target;
     NavMeshAgent agent;
     Animator animator;
+    public Collider punchCollider;
+    public Collider kickCollider;
 
     void Start()
     {
         animator = GetComponent<Animator>();  
         agent = GetComponent<NavMeshAgent>(); 
-        agent.destination = target.position; 
+        agent.destination = target.position;
+        HideColliderPunch();
+        HideColliderKick();
     }
 
     void Update()
     {
         agent.destination = target.position;
         animator.SetFloat("Distance", agent.remainingDistance); 
+    }
+
+    //攻撃の判定
+    public void HideColliderPunch()
+    {
+        punchCollider.enabled = false; //無効にする
+    }
+    public void ShowColliderPunch()
+    {
+        punchCollider.enabled = true;  //有効にする
+    }
+    public void HideColliderKick()
+    {
+        kickCollider.enabled = false; //無効にする
+    }
+    public void ShowColliderKick()
+    {
+        kickCollider.enabled = true;  //有効にする
     }
 
     private void OnTriggerEnter(Collider other)　// 当たり判定
